a28ccbc7f4d1ad3941b0e85fe43f2a422f0cbc87
Rin <rin.t.4921@gmail.com>
2021-09-11 17:57:59 +0900

    first commit

---
+++ PokeApiRxSwift/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PokeApiRxSwift
+//
+//  Created by Rin on 2021/09/11.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        print("aaaaa")
+    }
+
+
+}
+
